6cade497aefc17b7baf4490be4c168711a5230ba
Gian-Luca Fenocchi <lucabod8@gmail.com>
2022-11-22 05:00:43 +0000

    Initial Hyperparameter Search

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -6,23 +6,23 @@ import numpy as np
 import pandas as pd
 from enum import Enum
 import matplotlib.pyplot as plt
+import time
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import preprocessing, impute, metrics
 
+# class NormMethod(Enum):
+#     MIN_MAX = 0
+#     Z_SCORE = 1
 
-class NormMethod(Enum):
-    MIN_MAX = 0
-    Z_SCORE = 1
-
-
-class Loss(Enum):
-    MEAN_ABSOLUTE_ERROR = nn.L1Loss()
-    MEAN_SQUARE_ERROR = nn.MSELoss()
-    NEGATIVE_LOG_LIKELIHOOD = nn.NLLLoss()
-    CROSS_ENTROPY = nn.CrossEntropyLoss()
-    HINGE_EMBEDDING = nn.HingeEmbeddingLoss()
-    MARGIN_RANKING = nn.MarginRankingLoss()
-    TRIPLET_MARGIN = nn.TripletMarginLoss()
-    KULLBACK_LEIBLER_DIVERGENCE = nn.KLDivLoss()
-
+# class Loss(Enum):
+#     MEAN_ABSOLUTE_ERROR = nn.L1Loss()
+#     MEAN_SQUARE_ERROR = nn.MSELoss()
+#     NEGATIVE_LOG_LIKELIHOOD = nn.NLLLoss()
+#     CROSS_ENTROPY = nn.CrossEntropyLoss()
+#     HINGE_EMBEDDING = nn.HingeEmbeddingLoss()
+#     MARGIN_RANKING = nn.MarginRankingLoss()
+#     TRIPLET_MARGIN = nn.TripletMarginLoss()
+#     KULLBACK_LEIBLER_DIVERGENCE = nn.KLDivLoss()
 
 class Regressor:
     def __init__(
@@ -30,10 +30,10 @@ class Regressor:
         x,
         nb_epoch=1000,
         learning_rate=0.001,
-        normalisation_method=NormMethod.MIN_MAX,
-        loss_function=Loss.MEAN_SQUARE_ERROR,
-        batch_size=64,
-        plot_loss=True,
+        neurons=[8,8,8],
+        loss_function=nn.MSELoss(),
+        batch_size=1000,
+        plot_loss=False,
     ):
         # You can add any input parameters you need
         # Remember to set them with a default value for LabTS tests
@@ -48,27 +48,21 @@ class Regressor:
 
         """
 
-        #######################################################################
-        #                       ** START OF YOUR CODE **
-        #######################################################################
-
         # Replace this code with your own
+        self.__x = x
         self.__learning_rate = learning_rate
-        self.__normalisation_method = normalisation_method
         self.__loss_function = loss_function
-        self.__device = "cuda" if T.cuda.is_available() else "cpu"
+        # self.__device = T.device("cpu") 
+        self.__device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
+        T.cuda.set_device(self.__device)
         self.__batch_size = batch_size
         self.__plot_loss = plot_loss
 
         self.__training_columns = None
-        self.__x_mean = None
-        self.__y_mean = None
-        self.__x_std = None
-        self.__y_std = None
-        self.__x_min = None
-        self.__y_min = None
-        self.__x_max = None
-        self.__y_max = None
+        self.__label_replace = None
+        self.__x_imputer = impute.SimpleImputer(missing_values=np.nan, strategy="mean")
+        self.__x_scaling = preprocessing.MinMaxScaler()
+        self.__y_scaling = preprocessing.MinMaxScaler()
 
         X, _ = self._preprocessor(x, training=True)
         self.input_size = X.shape[1]
@@ -76,22 +70,23 @@ class Regressor:
         self.nb_epoch = nb_epoch
 
         # Network Initialisation
-        model = [
-            # nn.BatchNorm1d(self.input_size),
-            nn.Linear(self.input_size, 8),
-            nn.Linear(8, 8),
-            nn.Linear(8, 8),
-            nn.Linear(8, 8),
-            nn.Linear(8, self.output_size),
-        ]
+        self.__neurons = neurons
+        model = []
+        n_input = self.input_size
+        for layer in neurons:
+            model.append(nn.Linear(n_input, layer))
+            n_input = layer
+        model.append(nn.Linear(n_input, self.output_size))
+        model.append(nn.ReLU())
+
+
 
         for layer in model:
             if type(layer) == nn.Linear:
                 nn.init.xavier_uniform_(layer.weight)
                 layer.bias.data.fill_(0)
 
-        self.__network = nn.Sequential(*model)
-
+        self.__network = nn.Sequential(*model).to(self.__device)
         return
 
 
@@ -114,65 +109,42 @@ class Regressor:
 
         """
 
-        if not training:
-            if self.__training_columns is None:
-                raise Exception("You need to train the model first!")
-            if self.__normalisation_method == NormMethod.MIN_MAX:
-                if self.__x_min is None or self.__x_max is None:
-                    raise Exception("You need to train the model first!")
-            if self.__normalisation_method == NormMethod.Z_SCORE:
-                if self.__x_mean is None or self.__x_std is None:
-                    raise Exception("You need to train the model first!")
-
-        # One hot encoding of the discrete columns
-        for feature_column in x:
-            feature_values = x[feature_column]
-            if feature_values.dtype == "object":
-                new_columns = pd.get_dummies(feature_values)
-                x = x.drop(feature_column, axis=1).join(new_columns)
-
-        # Fill missing values with the mean of the column
-        x = x.fillna(x.mean())
-
-        # When training we initialise our normalisation values
         if training:
-            self.__x_mean = x.mean()
-            self.__x_std = x.std()
-            self.__x_min = x.min()
-            self.__x_max = x.max()
-            if y is not None:
-                self.__y_mean = y.mean()
-                self.__y_std = y.std()
-                self.__y_min = y.min()
-                self.__y_max = y.max()
+            self.__label_replace = x['ocean_proximity'].mode()[0]
+                        
+        # Transform the ocean_proximity column into the one-hot encoded columns.
+        x["ocean_proximity"] = x.loc[:, ['ocean_proximity']].fillna(value=self.__label_replace)
+        new_columns = pd.get_dummies(x["ocean_proximity"])
+        x = x.drop("ocean_proximity", axis=1).join(new_columns)
 
-
-        # For Z-Score Normalisation
-        if self.__normalisation_method == NormMethod.Z_SCORE:
-                x = (x - self.__x_mean) / self.__x_std
-                if y is not None:
-                    y = (y - self.__y_mean) / self.__y_std
-        # For Min-Max Normalisation
-        elif self.__normalisation_method == NormMethod.MIN_MAX:
-                x = (x - self.__x_min) / (self.__x_max - self.__x_min)
-                if y is not None:
-                    y = (y - self.__y_min) / (self.__y_max - self.__y_min)
-
-        # Putting the columns correct for the test dataset, or setting them for the training
+         # Putting the columns correct for the test dataset, or setting them for the training
         if training:
             self.__training_columns = x.columns
         else:
             x = x.reindex(columns=self.__training_columns, fill_value=0)
 
+        if training:
+            self.__x_imputer.fit(x)
+
+        x = self.__x_imputer.transform(x)
+        if training:
+            self.__x_scaling.fit(x)
+            if isinstance(y, pd.DataFrame):
+                self.__y_scaling.fit(y)
+    
+
         # Set X and Y types
         x = x.astype("float32")
         if y is not None:
             y = y.astype("float32")
+        
+        x = T.from_numpy(np.array(self.__x_scaling.transform(x))).to(self.__device)
+        y = T.from_numpy(np.array(self.__y_scaling.transform(y))).to(self.__device) if isinstance(y, pd.DataFrame) else None
+        
 
-        # Return preprocessed x and y, return None for y if it was None
-        return x, (y if isinstance(y, pd.DataFrame) else None)
+        return x, y
 
-    def fit(self, x, y):
+    def fit(self, x, y, dev_x=None, dev_y=None):
         """
         Regressor training function
 
@@ -187,28 +159,26 @@ class Regressor:
         """
 
         X, Y = self._preprocessor(x, y=y, training=True)  # Do not forget
-
-        x_tensor = T.tensor(np.array(X))
-        Y_Gold = T.tensor(np.array(Y))
-
-        data_batch = T.utils.data.TensorDataset(x_tensor, Y_Gold)
+        data_batch = T.utils.data.TensorDataset(X, Y)
         trainloader = T.utils.data.DataLoader(
             data_batch,
             batch_size=self.__batch_size,
             shuffle=True,
-            num_workers=1,
         )
 
         permutation = T.randperm(X.shape[0])
         loss_by_epoch = []
 
         optimiser = T.optim.Adam(self.__network.parameters(), lr=self.__learning_rate)
-
+        
         for i in range(self.nb_epoch):
 
             current_loss = 0
 
             for j, (data, data_y) in enumerate(trainloader):
+                data = data.to(self.__device)
+                data_y = data_y.to(self.__device)
+
                 # Shuffle
                 optimiser.zero_grad()
 
@@ -216,6 +186,9 @@ class Regressor:
                 Y_Pred = self.__network(data)
 
                 """print(
+                    "Batch:\t",
+                    j,
+                    "\t\t",
                     "Data:\t",
                     int(data_y.mean().item()),
                     "\t\t",
@@ -226,9 +199,9 @@ class Regressor:
                     int(data_y.mean().item()) - int(Y_Pred.mean().item()),
                 )"""
                 # Loss
-                loss = self.__loss_function.value(Y_Pred, data_y)
+                loss = self.__loss_function(Y_Pred, data_y)
 
-                current_loss += loss.item() / self.__batch_size
+                current_loss += loss.item()
 
                 # Backward
                 loss.backward()
@@ -238,8 +211,7 @@ class Regressor:
 
                 # Print statistics
 
-            if (i + 1) % 1 == 0:
-                print(f"Epoch {i+1}/{self.nb_epoch}, Train Loss: {current_loss:.4f}")
+            # print(f"Epoch {i+1}/{self.nb_epoch}, Train Loss: {current_loss:.4f}")
 
             loss_by_epoch.append(current_loss)
 
@@ -266,8 +238,7 @@ class Regressor:
         """
 
         X, _ = self._preprocessor(x, training=False)  # Do not forget
-        x_tensor = T.tensor(np.array(X))
-        return self.__network(x_tensor).detach().numpy()
+        return self.__network(X).cpu().detach().numpy()
 
     def score(self, x, y):
         """
@@ -283,12 +254,26 @@ class Regressor:
 
         """
 
-        X, Y = self._preprocessor(x, y=y, training=False)  # Do not forget
-        Y_Pred = self.__network(T.tensor(np.array(X)))
-        Y_Gold = T.tensor(np.array(Y))
+        X, Y = self._preprocessor(x, y=y, training=False) # Do not forget
+        Y_Pred = self.__network(X)
 
-        return self.__loss_function.value(Y_Pred, Y_Gold).item()
+        return self.__loss_function(Y_Pred, Y).item()
 
+
+
+    def get_params(self, deep=True):
+        return {
+            'x': self.__x,
+            'learning_rate': self.__learning_rate,
+            'nb_epoch': self.nb_epoch,
+            'neurons': self.__neurons,
+            'batch_size': self.__batch_size
+        }
+
+    def set_params(self, **params):
+        for p, v in params.items():
+            setattr(self, p, v)
+        return self
 
 def save_regressor(trained_model):
     """
@@ -311,7 +296,7 @@ def load_regressor():
     return trained_model
 
 
-def RegressorHyperParameterSearch():
+def RegressorHyperParameterSearch(params):
     # Ensure to add whatever inputs you deem necessary to this function
     """
     Performs a hyper-parameter for fine-tuning the regressor implemented
@@ -324,37 +309,38 @@ def RegressorHyperParameterSearch():
         The function should return your optimised hyper-parameters.
 
     """
-    best_learning_rate = 0
-    lowest_error = 10000000000
+    from tune_sklearn import TuneGridSearchCV
 
-    output_label = "median_house_value"
-    data = pd.read_csv("housing.csv")
-    total_samples = len(data)
-    train_fraction = 0.8
-    si = train_fraction * total_samples
+    x, y = read_dataset("housing.csv", "median_house_value")
+    X_train, X_test, y_train, y_test = train_test_split(x, y,
+                                random_state=104, 
+                                test_size=0.25, 
+                                shuffle=True)
 
-    x_train = data.loc[:si, data.columns != output_label]
-    y_train = data.loc[:si, [output_label]]
-    x_test = data.loc[si:, data.columns != output_label]
-    y_test = data.loc[si:, [output_label]]
+    # grid_search = TuneGridSearchCV(
+    grid_search = GridSearchCV(
+        Regressor(x=X_train, nb_epoch=10),
+        param_grid=params,
+        scoring="neg_root_mean_squared_error",
+        n_jobs=-1,
+        verbose=2,
+        return_train_score=True,
+        # use_gpu=True,
+    )
+    
+    grid_search.fit(X_train, y_train, dev_x=X_test, dev_y=y_test)
 
-    for i in np.arange(0.01, 0.1, 0.01):
-        regressor = Regressor(x_train, nb_epoch=10, learning_rate=i)
-        regressor.fit(x_train, y_train)
-        save_regressor(regressor)
+    print("Best params:", grid_search.best_params_)
 
-        # Error
-        error = regressor.score(x_train, y_train)
-        print("\nTrain Regressor error: {}\n".format(error))
+    # analysis = tune.run(train, config={"lr": tune.grid_search([0.001, 0.01, 0.1])}, resources_per_trial={"cpu": 28, "gpu": 1})
+    # print("Best Config:", analysis.get_best_config(metric="mean_accuracy"))
 
-        error_test = regressor.score(x_test, y_test)
-        print("\nTest Regressor error: {}\n".format(error_test))
 
-        if error < lowest_error:
-            best_learning_rate = i
-            lowest_error = error
-
-    print(best_learning_rate)
+def read_dataset(path, output_label):
+    data = pd.read_csv(path)
+    x = data.loc[:, data.columns != output_label]
+    y = data.loc[:, [output_label]]
+    return x, y
 
 
 def example_main():
@@ -386,12 +372,19 @@ def example_main():
 
     # Error
     error = regressor.score(x_train, y_train)
-    print("\nTrain Regressor error: {}\n".format(error))
+    print("Train Regressor error: {}".format(error))
 
     error_test = regressor.score(x_test, y_test)
-    print("\nTest Regressor error: {}\n".format(error_test))
+    print("Test Regressor error: {}".format(error_test))
 
-    RegressorHyperParameterSearch()
+
+    params = {
+        'learning_rate': [0.001, 0.005, 0.05, 0.1],
+        'batch_size': [64, 256, 512, 1024],
+        'neurons': [[13], [10, 10], [8, 8], [8, 8, 8], [13, 8], [9, 4], [13, 9, 4]]
+    }
+
+    RegressorHyperParameterSearch(params)
 
 
 
